76230f822b817f27c8f43d5342514f5de4a303d5
homile <ssanho2@naver.com>
2022-04-05 12:37:25 +0900

    state 학습내용

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -1,48 +1,29 @@
 import React, {Component} from 'react';
 import './App.css';
-
-class Subject extends Component{
-   render(){
-      return (
-         <header>
-            <h1>{this.props.title}</h1>
-            {this.props.sub}
-         </header>
-      );
-   }
-}
-
-class TOC extends Component{
-   render(){
-      return(
-         <nav>
-            <ul>
-               <li><a href="1.html">HTML</a></li>
-               <li><a href="2.html">CSS</a></li>
-               <li><a href="3.html">JavaScript</a></li>
-            </ul>
-         </nav>
-      );
-   }
-}
-
-class Content extends Component{
-   render(){
-      return(
-         <article>
-            <h2>{this.props.title}</h2>
-            {this.props.desc}
-         </article>  
-      );
-   }
-}
+import TOC from './components/TOC'
+import Content from './components/Content';
+import Subject from './components/Subject';
 
 class App extends Component{
+   constructor(props){
+      super(props);
+      this.state = {
+         subject:{title:'WEB', sub:'World Wide Web!'},
+         contents:[
+            {id:1, title:'HTML', desc:'HTML is HyperText Markup Language'},
+            {id:2, title:'CSS', desc:'CSS is for design'},
+            {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+         ]
+      }
+   }
    render(){
       return (
          <div className="App">
-            <Subject title="WEB" sub="world wid web!"></Subject>
-            <TOC></TOC>
+            <Subject 
+               title={this.state.subject.title} 
+               sub={this.state.subject.sub}>
+            </Subject>
+            <TOC data={this.state.contents}></TOC>
             <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
          </div>
       );
---
+++ React/react-app/src/components/TOC.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+class TOC extends Component{
+   render(){
+      let lists = [];
+      let data = this.props.data;
+      let i = 0;
+      while (i < data.length){
+         // 여러개의 목록을 자동으로 생성할 때에는 key와 식별자를 적어준다.
+         lists.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>)
+         i = i + 1;
+      }
+      return(
+         <nav>
+            <ul>
+               {lists}
+            </ul>
+         </nav>
+      );
+   }
+}
+
+// TOC라는 클래스를 다른 클래스에서 사용할 수 있게 해준다.
+export default TOC;
