4aa9799bf5f97a3502e1c44f653096731a26b4b1
homile <ssanho2@naver.com>
2022-04-10 13:48:51 +0900

    Create 구현1

--- React/react-app/src/App.js
+++ React/react-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TOC from './components/TOC'
 import Content from './components/Content';
 import Subject from './components/Subject';
+import Control from './components/Control';
 
 class App extends Component{
    constructor(props){
@@ -54,6 +55,12 @@ class App extends Component{
                });
             }.bind(this)}
             data={this.state.contents}></TOC>
+
+            <Control onChangeMode={function(_mode){
+               this.setState({
+                  mode:_mode
+               })
+            }.bind(this)}></Control>
 
             <Content title={_title} desc={_desc}></Content>
          </div>
